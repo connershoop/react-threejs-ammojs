8327403d388a4685642a6febbd04a917f64c2b9f
Conner Shoop <connershoop@pop-os.localdomain>
2021-06-21 22:09:50 -0400

    add ammojs to npm packagejs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import Box from './Components/Box';
+import { default as Ammo } from 'ammo.js/builds/ammo';
 
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => (mesh.current.rotation.x += 0.01))
+//Ammojs Initialization
+Ammo().then( start )
+        
+function start(){
+  console.log('test')
+    //code goes here
 
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 2, 3]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
 }
-
 
 function App() {
   return (
